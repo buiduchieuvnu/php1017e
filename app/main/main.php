6b94d0565e3d7afd3e09d951517c7a66e970a815
buiduchieuvnu <buiduchieuvnu@gmail.com>
2018-03-27 18:51:36 +0700

    Thay skin admin trananh Signed-off-by: buiduchieuvnu <buiduchieuvnu@gmail.com>

--- app/main/main.php
+++ app/main/main.php
@@ -5,15 +5,14 @@ if ( !defined('AREA') ) { die('Access denied'); }
 <?php
 class MainApp extends AppObject{
 	public $app_name="main";
-	public $dir_layout="backend"; // thư mục chứa các layout
 	public function __construct(){
-		parent::__construct();
+            parent::__construct();
 	}
 	public function display(){
-		$this->dir_layout="backend";
-		$this->layout="default";
-		$this->view="default";
-		parent::display();
+            $this->dir_layout="admin_trananh";
+            $this->layout="layout_admin";
+            $this->view="bangdieukhien";
+            parent::display();
 	}
 }
 ?>
