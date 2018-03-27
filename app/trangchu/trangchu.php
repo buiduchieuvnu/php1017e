f4479fad76624416116da75cbc80c0ae10f87060
buiduchieuvnu <buiduchieuvnu@gmail.com>
2018-03-27 18:52:29 +0700

    Trang chủ traanh.vn Signed-off-by: buiduchieuvnu <buiduchieuvnu@gmail.com>

--- app/trangchu/trangchu.php
+++ app/trangchu/trangchu.php
@@ -10,6 +10,14 @@ class TrangchuApp extends AppObject{
             $this->dir_layout="frontend";
             $this->layout="layout_trangchu";
             $this->view="default";
+            
+            // Khởi tạo đối tượng xử lý dữ liệu
+            $DB = new Database();
+            // Lấy dữ liệu danh mục con của dm tivi
+            $this->DM_TIVI = $DB->queryAll("CALL dm_hang_tim_theo_cha(12)");
+            // Lấy ra danh sách sản phẩm thuộc danh mục tivi
+            $this->SP_TIVI = $DB->queryAll("CALL san_pham_tim_theo_dm(12)");
+            
             parent::display();
 	}
 }
--- app/trangchu/views/default.php
+++ app/trangchu/views/default.php
@@ -1,257 +1,47 @@
+<?php
+// Test dữ liệu:
+//echo '<pre>';
+//print_r($this->DM_TIVI);
+//print_r($this->SP_TIVI);
+?>
+
 <div class="dong_sanpham">
     <div class="danh_muc">
         <h3><a href="#">Tivi</a></h3>
         <ul>
-            <li><a href="#">Tivi Sony </a></li>
-            <li><a href="#">Tivi Samsung </a></li>
-            <li><a href="#">Tivi LG </a></li>
-            <li><a href="#">Android Tivi </a></li>
-            <li><a href="#">Tivi 32-43 Inch </a></li>
-            <li><a href="#">Tivi Full HD </a></li>
-            <li><a href="#">Smart Tivi </a></li>
-            <li><a href="#">Tivi 4K </a></li>
-            <li><a href="#">Tivi HDR </a></li>
-            <li><a href="#">Đầu DVD </a></li>
-            <li><a href="#">Cáp HDMI </a></li>
+            <?php
+            foreach ($this->DM_TIVI as $key => $value) {
+            ?>
+                <li><a href="#"><?=$value['ten_danh_muc']?> </a></li>
+            <?php
+            }
+            ?>
+            
         </ul>
         <a class="xem_them" href="#">View more ></a>
     </div>
     <ul class="danhsach_sanpham">
+        <?php
+            foreach ($this->SP_TIVI as $key => $value) {
+            ?>
         <li>
             <a href="#">
-                <img class="anh_sanpham" src="skins/frontend/images/sanpham/tivi_1.png" />
+                <img class="anh_sanpham" src="skins/frontend/images/sanpham/<?=$value['hinh_anh']?>" />
                 <div class="dong_1">
                     <img class="logo_hangsx" src="skins/frontend/images/logo/logo_samsung.png"/>
-                    <h3 class="loai_sanpham">Smart TV</h3>
-                    <span class="ma_sanpham">(UA43M5500)</span>
+                    <h3 class="loai_sanpham"><?=$value['ten_san_pham']?></h3>
+                    <span class="ma_sanpham">(<?=$value['ma_san_phan']?>)</span>
                 </div>
                 <div class="dong_2">
-                    <span class="gia_sanpham">12.390.000 <sup>đ</sup></span>
+                    <span class="gia_sanpham"><?=$value['gia_ban']?> <sup>đ</sup></span>
                     <span class="tra_gop"></span>
                 </div>
 
                 <p class="khuyen_mai">Có 2 khuyến mại</p>
             </a>
         </li>
-        <li>
-            <a href="#">
-                <img class="anh_sanpham" src="skins/frontend/images/sanpham/tivi_1.png" />
-                <div class="dong_1">
-                    <img class="logo_hangsx" src="skins/frontend/images/logo/logo_samsung.png"/>
-                    <h3 class="loai_sanpham">Smart TV</h3>
-                    <span class="ma_sanpham">(UA43M5500)</span>
-                </div>
-                <div class="dong_2">
-                    <span class="gia_sanpham">12.390.000 <sup>đ</sup></span>
-                    <span class="tra_gop"></span>
-                </div>
-
-                <p class="khuyen_mai">Có 2 khuyến mại</p>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <img class="anh_sanpham" src="skins/frontend/images/sanpham/tivi_1.png" />
-                <div class="dong_1">
-                    <img class="logo_hangsx" src="skins/frontend/images/logo/logo_samsung.png"/>
-                    <h3 class="loai_sanpham">Smart TV</h3>
-                    <span class="ma_sanpham">(UA43M5500)</span>
-                </div>
-                <div class="dong_2">
-                    <span class="gia_sanpham">12.390.000 <sup>đ</sup></span>
-                    <span class="tra_gop"></span>
-                </div>
-
-                <p class="khuyen_mai">Có 2 khuyến mại</p>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <img class="anh_sanpham" src="skins/frontend/images/sanpham/tivi_1.png" />
-                <div class="dong_1">
-                    <img class="logo_hangsx" src="skins/frontend/images/logo/logo_samsung.png"/>
-                    <h3 class="loai_sanpham">Smart TV</h3>
-                    <span class="ma_sanpham">(UA43M5500)</span>
-                </div>
-                <div class="dong_2">
-                    <span class="gia_sanpham">12.390.000 <sup>đ</sup></span>
-                    <span class="tra_gop"></span>
-                </div>
-
-                <p class="khuyen_mai">Có 2 khuyến mại</p>
-            </a>
-        </li>
-
-    </ul>
-</div>
-<div class="dong_sanpham">
-    <div class="danh_muc">
-        <h3><a href="#">Tivi</a></h3>
-        <ul>
-            <li><a href="#">Tivi Sony </a></li>
-            <li><a href="#">Tivi Samsung </a></li>
-            <li><a href="#">Tivi LG </a></li>
-            <li><a href="#">Android Tivi </a></li>
-            <li><a href="#">Tivi 32-43 Inch </a></li>
-            <li><a href="#">Tivi Full HD </a></li>
-            <li><a href="#">Smart Tivi </a></li>
-            <li><a href="#">Tivi 4K </a></li>
-            <li><a href="#">Tivi HDR </a></li>
-            <li><a href="#">Đầu DVD </a></li>
-            <li><a href="#">Cáp HDMI </a></li>
-        </ul>
-        <a class="xem_them" href="#">View more ></a>
-    </div>
-    <ul class="danhsach_sanpham">
-        <li>
-            <a href="#">
-                <img class="anh_sanpham" src="skins/frontend/images/sanpham/tivi_1.png" />
-                <div class="dong_1">
-                    <img class="logo_hangsx" src="skins/frontend/images/logo/logo_samsung.png"/>
-                    <h3 class="loai_sanpham">Smart TV</h3>
-                    <span class="ma_sanpham">(UA43M5500)</span>
-                </div>
-                <div class="dong_2">
-                    <span class="gia_sanpham">12.390.000 <sup>đ</sup></span>
-                    <span class="tra_gop"></span>
-                </div>
-
-                <p class="khuyen_mai">Có 2 khuyến mại</p>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <img class="anh_sanpham" src="skins/frontend/images/sanpham/tivi_1.png" />
-                <div class="dong_1">
-                    <img class="logo_hangsx" src="skins/frontend/images/logo/logo_samsung.png"/>
-                    <h3 class="loai_sanpham">Smart TV</h3>
-                    <span class="ma_sanpham">(UA43M5500)</span>
-                </div>
-                <div class="dong_2">
-                    <span class="gia_sanpham">12.390.000 <sup>đ</sup></span>
-                    <span class="tra_gop"></span>
-                </div>
-
-                <p class="khuyen_mai">Có 2 khuyến mại</p>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <img class="anh_sanpham" src="skins/frontend/images/sanpham/tivi_1.png" />
-                <div class="dong_1">
-                    <img class="logo_hangsx" src="skins/frontend/images/logo/logo_samsung.png"/>
-                    <h3 class="loai_sanpham">Smart TV</h3>
-                    <span class="ma_sanpham">(UA43M5500)</span>
-                </div>
-                <div class="dong_2">
-                    <span class="gia_sanpham">12.390.000 <sup>đ</sup></span>
-                    <span class="tra_gop"></span>
-                </div>
-
-                <p class="khuyen_mai">Có 2 khuyến mại</p>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <img class="anh_sanpham" src="skins/frontend/images/sanpham/tivi_1.png" />
-                <div class="dong_1">
-                    <img class="logo_hangsx" src="skins/frontend/images/logo/logo_samsung.png"/>
-                    <h3 class="loai_sanpham">Smart TV</h3>
-                    <span class="ma_sanpham">(UA43M5500)</span>
-                </div>
-                <div class="dong_2">
-                    <span class="gia_sanpham">12.390.000 <sup>đ</sup></span>
-                    <span class="tra_gop"></span>
-                </div>
-
-                <p class="khuyen_mai">Có 2 khuyến mại</p>
-            </a>
-        </li>
-
-    </ul>
-</div>
-<div class="dong_sanpham">
-    <div class="danh_muc">
-        <h3><a href="#">Tivi</a></h3>
-        <ul>
-            <li><a href="#">Tivi Sony </a></li>
-            <li><a href="#">Tivi Samsung </a></li>
-            <li><a href="#">Tivi LG </a></li>
-            <li><a href="#">Android Tivi </a></li>
-            <li><a href="#">Tivi 32-43 Inch </a></li>
-            <li><a href="#">Tivi Full HD </a></li>
-            <li><a href="#">Smart Tivi </a></li>
-            <li><a href="#">Tivi 4K </a></li>
-            <li><a href="#">Tivi HDR </a></li>
-            <li><a href="#">Đầu DVD </a></li>
-            <li><a href="#">Cáp HDMI </a></li>
-        </ul>
-        <a class="xem_them" href="#">View more ></a>
-    </div>
-    <ul class="danhsach_sanpham">
-        <li>
-            <a href="#">
-                <img class="anh_sanpham" src="skins/frontend/images/sanpham/tivi_1.png" />
-                <div class="dong_1">
-                    <img class="logo_hangsx" src="skins/frontend/images/logo/logo_samsung.png"/>
-                    <h3 class="loai_sanpham">Smart TV</h3>
-                    <span class="ma_sanpham">(UA43M5500)</span>
-                </div>
-                <div class="dong_2">
-                    <span class="gia_sanpham">12.390.000 <sup>đ</sup></span>
-                    <span class="tra_gop"></span>
-                </div>
-
-                <p class="khuyen_mai">Có 2 khuyến mại</p>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <img class="anh_sanpham" src="skins/frontend/images/sanpham/tivi_1.png" />
-                <div class="dong_1">
-                    <img class="logo_hangsx" src="skins/frontend/images/logo/logo_samsung.png"/>
-                    <h3 class="loai_sanpham">Smart TV</h3>
-                    <span class="ma_sanpham">(UA43M5500)</span>
-                </div>
-                <div class="dong_2">
-                    <span class="gia_sanpham">12.390.000 <sup>đ</sup></span>
-                    <span class="tra_gop"></span>
-                </div>
-
-                <p class="khuyen_mai">Có 2 khuyến mại</p>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <img class="anh_sanpham" src="skins/frontend/images/sanpham/tivi_1.png" />
-                <div class="dong_1">
-                    <img class="logo_hangsx" src="skins/frontend/images/logo/logo_samsung.png"/>
-                    <h3 class="loai_sanpham">Smart TV</h3>
-                    <span class="ma_sanpham">(UA43M5500)</span>
-                </div>
-                <div class="dong_2">
-                    <span class="gia_sanpham">12.390.000 <sup>đ</sup></span>
-                    <span class="tra_gop"></span>
-                </div>
-
-                <p class="khuyen_mai">Có 2 khuyến mại</p>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <img class="anh_sanpham" src="skins/frontend/images/sanpham/tivi_1.png" />
-                <div class="dong_1">
-                    <img class="logo_hangsx" src="skins/frontend/images/logo/logo_samsung.png"/>
-                    <h3 class="loai_sanpham">Smart TV</h3>
-                    <span class="ma_sanpham">(UA43M5500)</span>
-                </div>
-                <div class="dong_2">
-                    <span class="gia_sanpham">12.390.000 <sup>đ</sup></span>
-                    <span class="tra_gop"></span>
-                </div>
-
-                <p class="khuyen_mai">Có 2 khuyến mại</p>
-            </a>
-        </li>
+        <?php
+            }
+            ?>
     </ul>
 </div>
