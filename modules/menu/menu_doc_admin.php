bc91dad15c3f3fcd627da3f8f0da7bf829dec77b
buiduchieuvnu <buiduchieuvnu@gmail.com>
2018-03-27 20:03:18 +0700

    Bổ xung menu
    Signed-off-by: buiduchieuvnu <buiduchieuvnu@gmail.com>

---
+++ modules/menu/menu_doc_admin.php
@@ -0,0 +1,110 @@
+<div class="navbar-default sidebar" role="navigation">
+    <div class="sidebar-nav navbar-collapse">
+        <ul class="nav" id="side-menu">
+            <li class="sidebar-search">
+                <div class="input-group custom-search-form">
+                    <input type="text" class="form-control" placeholder="Tìm kiếm...">
+                    <span class="input-group-btn">
+                        <button class="btn btn-default" type="button">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </span>
+                </div>
+                <!-- /input-group -->
+            </li>
+            <li>
+                <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Bảng điều khiển</a>
+            </li>
+            <li>
+                <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> QL danh mục<span class="fa arrow"></span></a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="?app=dm_hang">Danh mục hàng hóa</a>
+                    </li>
+                    <li>
+                        <a href="danhmuc_ncc.html">Danh mục nhà cung cấp</a>
+                    </li>
+                    <li>
+                        <a href="danhmuc_ncc.html">Danh mục thương hiệu</a>
+                    </li>
+                    <li>
+                        <a href="danhmuc_ncc.html">Danh mục chi nhánh</a>
+                    </li>
+                    <li>
+                        <a href="danhmuc_ncc.html">Danh mục tỉnh thành</a>
+                    </li>
+                    <li>
+                        <a href="danhmuc_ncc.html">Chuyên mục bài viết</a>
+                    </li>
+                </ul>
+                <!-- /.nav-second-level -->
+            </li>
+            <li>
+                <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> QL bán hàng<span class="fa arrow"></span></a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="danhmuc_hanghoa.html">Danh mục sản phẩm</a>
+                    </li>
+                    <li>
+                        <a href="danhmuc_ncc.html">Đơn đặt hàng</a>
+                    </li>
+                    <li>
+                        <a href="danhmuc_ncc.html">Nhập kho</a>
+                    </li>
+                    <li>
+                        <a href="danhmuc_ncc.html">Xuất kho</a>
+                    </li>
+                    <li>
+                        <a href="danhmuc_ncc.html">QL tồn kho</a>
+                    </li>
+                    <li>
+                        <a href="danhmuc_ncc.html">Khuyến mại - giảm giá</a>
+                    </li>
+                </ul>
+                <!-- /.nav-second-level -->
+            </li>
+            <li>
+                <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> QL cấu hình website<span class="fa arrow"></span></a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="danhmuc_hanghoa.html">Cấu hình menu</a>
+                    </li>
+                    <li>
+                        <a href="danhmuc_ncc.html">Cấu hình quảng cáo</a>
+                    </li>
+                    <li>
+                        <a href="danhmuc_ncc.html">Cấu hình slide</a>
+                    </li>
+                    <li>
+                        <a href="danhmuc_ncc.html">Cấu hình chung</a>
+                    </li>
+                </ul>
+                <!-- /.nav-second-level -->
+            </li>
+            <li>
+                <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Quản trị hệ thống<span class="fa arrow"></span></a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="danhmuc_hanghoa.html">QL Tài khoản</a>
+                    </li>
+                    <li>
+                        <a href="danhmuc_hanghoa.html">QL Sao lưu - khôi phục</a>
+                    </li>
+                    <li>
+                        <a href="danhmuc_ncc.html">QL góp ý - liên hệ</a>
+                    </li>
+                    <li>
+                        <a href="danhmuc_ncc.html">QL bản đồ chi nhánh</a>
+                    </li>
+                    <li>
+                        <a href="danhmuc_ncc.html">QL lịch sử hoạt đông HT</a>
+                    </li>
+                </ul>
+                <!-- /.nav-second-level -->
+            </li>
+
+            </li>
+        </ul>
+    </div>
+    <!-- /.sidebar-collapse -->
+</div>
